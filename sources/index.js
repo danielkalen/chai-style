053d3a3fb6efa2b10333971c220441d2d6fc9acd
danielkalen <d@danielkalen.com>
2017-08-15 21:33:08 -0400

    avoid creating extra elements when possible

--- sources/index.js
+++ sources/index.js
@@ -3,6 +3,7 @@ module.exports = chaiStyle
 function chaiStyle(chai, utils) {
   const {Assertion} = chai
   const {flag} = utils
+  let sampleDiv, sampleStyle, sampleIframe
 
   Assertion.addMethod('style', function(property, value = '') {
     const element = flag(this, 'object')
@@ -29,18 +30,22 @@ function chaiStyle(chai, utils) {
 
     function compareCSSValue(computed, expected) {
       const propertyHifenCase = property.replace(/[A-Z]/g, (match) => '-' + match.toLowerCase())
-      const fake = document.createElement('div')
-      fake.style.fontSize = style.fontSize
-      fake.style.setProperty(propertyHifenCase, expected, 'important')
-      const iframe = document.createElement('iframe')
-      iframe.style.visibility = 'hidden'
-      document.body.appendChild(iframe)
-      iframe.appendChild(fake)
-      const fakeStyle = window.getComputedStyle(fake)
-      const value = fakeStyle[property]
+      if (!sampleDiv) {
+        sampleIframe = document.createElement('iframe')
+        sampleDiv = document.createElement('div')
+        sampleStyle = window.getComputedStyle(sampleDiv)
+
+        sampleIframe.appendChild(sampleDiv)
+        document.body.appendChild(sampleIframe)
+      }
+      sampleDiv.style.fontSize = style.fontSize
+      sampleDiv.style.setProperty(propertyHifenCase, expected, 'important')
+      const value = sampleStyle[property]
 
       const hasAutoValue = value.includes('auto')
       const reg = new RegExp(escapeRegExp(value).replace(/auto/g, '(\\d+(.\\d+)?px|auto)'))
+      sampleDiv.style.fontSize = null
+      sampleDiv.style[propertyHifenCase] = null
 
       return hasAutoValue
         ? reg.test(computed)
